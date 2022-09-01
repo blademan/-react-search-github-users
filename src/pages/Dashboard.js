5ff98430b8c992cfa75ebb3f2443a08d221bcac7
Eduard <blademan82@gmail.com>
2022-09-01 15:44:55 +0300

    add info section

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,11 +3,16 @@ import { Info, Repos, User, Search, Navbar } from '../components';
 import loadingImage from '../images/preloader.gif';
 import { GithubContext } from '../context/context';
 const Dashboard = () => {
-  return (
-    <main>
-      <h2>Dashboard Page</h2>
-    </main>
-  );
+ return (
+  <main>
+   {/* <Navbar></Navbar> */}
+   {/* <Search /> */}
+   <Info />
+
+   <User />
+   {/* <Repos /> */}
+  </main>
+ );
 };
 
 export default Dashboard;
